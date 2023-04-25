a91b725fe22292621cc49c5449f90977b2fa55d3
AnkitKumar <AKumar@cnxridc.com>
2023-04-25 13:10:49 +0530

    locust-framework

--- parallel.py
+++ parallel.py
@@ -12,7 +12,7 @@ def run_test(test_file):
     report_file = os.path.join(report_folder, f"report_{timestamp}.html")
     print(f"Starting test file: {test_file}")
     subprocess.run(
-        ["locust", "-f", test_file,"--host=https://reqres.in","--headless", "-u", "1000", "-r", "1", "-t", "400s", "--html", report_file,"--logfile","ParallelTestcases.log"])
+        ["locust", "-f", test_file,"--host=https://reqres.in","--headless", "-u", "1000", "-r", "25", "-t", "60s", "--html", report_file,"--logfile","ParallelTestcases.log"])
     print(f"Finished test file: {test_file}")
 
     report_files = [os.path.join(report_folder, f) for f in os.listdir(report_folder) if f.endswith('.html')]
